15cf201fe339f2d6e7e165f636a282fde2ef83ad
Edgar Seijo <edgar.seijo@gmail.com>
2020-05-08 18:59:11 +0200

    Updates

---
+++ precache-manifest.ca5825c97b043770d2e45565804a24a0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaba307b7fadec3c1522fc059ae7f5d9",
+    "url": "/web-test/static/media/logo_no_name.aaba307b.png"
+  },
+  {
+    "revision": "4bb47a0cbbb3c1f99449",
+    "url": "/web-test/static/css/main.e22ac58a.chunk.css"
+  },
+  {
+    "revision": "b13c75971e64f6d85d03",
+    "url": "/web-test/static/js/runtime~main.4c29f8fe.js"
+  },
+  {
+    "revision": "417f69cd153b90671e74",
+    "url": "/web-test/static/js/2.abc09aff.chunk.js"
+  },
+  {
+    "revision": "df600be62b8a1c48267c263cef4ecdd5",
+    "url": "/web-test/static/media/logo.df600be6.png"
+  },
+  {
+    "revision": "30f43d516b9f5fa3adda8168c52e8ba3",
+    "url": "/web-test/static/media/profile.30f43d51.jpg"
+  },
+  {
+    "revision": "40fc1ae06bb61d91fa0ffd9b9687de3e",
+    "url": "/web-test/static/media/profile_alt.40fc1ae0.jpg"
+  },
+  {
+    "revision": "4bb47a0cbbb3c1f99449",
+    "url": "/web-test/static/js/main.05145502.chunk.js"
+  },
+  {
+    "revision": "0a05286670d723c21b7d33d90639fff5",
+    "url": "/web-test/static/media/main_background_light.0a052866.jpg"
+  },
+  {
+    "revision": "5b6e4d53583cb5080cddeddcac2252e2",
+    "url": "/web-test/static/media/personalized_consultation1_alt.5b6e4d53.png"
+  },
+  {
+    "revision": "941c5993572de38144d8bdfa13bdfe63",
+    "url": "/web-test/static/media/workshops1.941c5993.png"
+  },
+  {
+    "revision": "4765a7be89655d2b06d17c9e9bc5aab7",
+    "url": "/web-test/static/media/workshops1_alt.4765a7be.png"
+  },
+  {
+    "revision": "0f8ac1b5671ab9228bc99b34087a7f77",
+    "url": "/web-test/static/media/colaborations.0f8ac1b5.png"
+  },
+  {
+    "revision": "4f124169185f0c7fceec9d0e2a30f236",
+    "url": "/web-test/static/media/colaborations_alt.4f124169.png"
+  },
+  {
+    "revision": "f429b7a62d2d486c4c2adcfe819dd1ca",
+    "url": "/web-test/index.html"
+  }
+];
